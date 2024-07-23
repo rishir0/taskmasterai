da8467d2e2e851bf7ef5777d0b2ae74628124ec2
srini <srinibaj10@gmail.com>
2024-07-23 04:33:28 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import ProjectsTasksList from './ProjectsTasksList';
+
+const App = () => {
+  return (
+    <div>
+      <ProjectsTasksList />
+    </div>
+  );
+};
+
+export default App;
