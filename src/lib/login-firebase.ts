b9e2ca606a020ed65e963245295dffb9d6a00896
srini <srinibaj10@gmail.com>
2024-11-23 21:09:44 -0500

    Create login-firebase.ts

---
+++ src/lib/login-firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaMAlQRMXiDsZ4P0b06P18id3y5xBiZ1k",
+  authDomain: "deepworkai-c3419.firebaseapp.com",
+  projectId: "deepworkai-c3419",
+  storageBucket: "deepworkai-c3419.appspot.com",
+  messagingSenderId: "367439182644",
+  appId: "1:367439182644:web:304216430df97eff68c361"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
