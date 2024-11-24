0e17065579685ca11f19851107d9fc7354f7b570
srini <srinibaj10@gmail.com>
2024-11-23 21:13:06 -0500

    Create login-header.tsx

---
+++ src/components/login/login-header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function LoginHeader() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center mb-2 text-white">Login</h1>
+      <p className="text-gray-300 text-center mb-6">
+        Create notes in minutes. Free forever. No credit card required.
+      </p>
+    </>
+  );
+}
