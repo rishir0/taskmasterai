6ed113ff037d0654b4bcb8808c70bc71fdfa9b69
srini <srinibaj10@gmail.com>
2024-11-23 20:59:54 -0500

    Update loginmain.tsx

--- src/loginmain.tsx
+++ src/loginmain.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { AuthProvider } from './contexts/AuthContext';
 import { Login } from './components/Login';
 import './index.css';
 
