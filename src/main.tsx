4c1e0666b4a5481f58bd3409dfabcb52615d7426
srini <srinibaj10@gmail.com>
2024-11-24 20:43:27 -0500

    Update main.tsx 7

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<App />} /> {/* Default homepge route */}
+          <Route path="/" element={<App />} /> {/* Default  route */}
         </Routes>
       </AuthProvider>
     </BrowserRouter>
