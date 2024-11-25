24ba75d80064217350213eedae8aeee61599494a
srini <srinibaj10@gmail.com>
2024-11-24 20:50:41 -0500

    bump

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useAuth } from './contexts/AuthContext';
+import { useAuth } from '../contexts/AuthContext';
 import { Loader2 } from 'lucide-react';
 
 function Login() {
