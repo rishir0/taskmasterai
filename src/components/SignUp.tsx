925a831ae4e54d1b4be2a4b29249fa27e27462a3
srini <srinibaj10@gmail.com>
2024-11-27 15:43:06 -0500

    Update SignUp.tsx

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Loader2 } from 'lucide-react';
-import { useAuth } from '../../contexts/AuthContext';
+import { useAuth } from '../contexts/AuthContext';
 import { SignUpForm } from './SignUpForm';
 import { GoogleSignUp } from './GoogleSignUp';
 
