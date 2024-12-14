c6699b6103fb84e2d2eb20da235af989fffe3bb2
srini <srinibaj10@gmail.com>
2024-12-13 21:48:25 -0500

    Create Icons.tsx

---
+++ src/utils/Icons.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Icons = {
+  logo: (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      {/* Add SVG paths */}
+    </svg>
+  ),
+  dashboard: (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      {/* Add SVG paths */}
+    </svg>
+  ),
+  // Add other icons...
+};
