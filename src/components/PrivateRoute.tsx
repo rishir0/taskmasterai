d562ab5b754b88ec675d5f27f178790f003af471
srini <srinibaj10@gmail.com>
2024-12-13 21:53:18 -0500

    Create PrivateRoute.tsx

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+interface PrivateRouteProps {
+  children: React.ReactNode;
+}
+
+export const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+  const { currentUser, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return currentUser ? <>{children}</> : <Navigate to="/login" />;
+};
